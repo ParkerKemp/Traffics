49622747154cbcb6b298ec0c67bd4425757b7d2b
ParkerKemp <charles.kemp702@topper.wku.edu>
2014-10-20 14:44:03 -0500

    Add PlayerJoinEvent

--- src/com/spinalcraft/traffics/Traffics.java
+++ src/com/spinalcraft/traffics/Traffics.java
@@ -1,8 +1,15 @@
 package com.spinalcraft.traffics;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 import org.bukkit.Bukkit;
 import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.spinalcraft.spinalpack.Co;
@@ -15,7 +22,7 @@ public class Traffics extends JavaPlugin implements Listener{
 	@Override
 	public void onEnable(){
 		console = Bukkit.getConsoleSender();
-		console.sendMessage(Spinalpack.code(Co.BLUE) + "Slipdisk online!");
+		console.sendMessage(Spinalpack.code(Co.BLUE) + "Traffics online!");
 		getServer().getPluginManager().registerEvents((Listener)this,  this);
 		
 	}
@@ -23,5 +30,12 @@ public class Traffics extends JavaPlugin implements Listener{
 	@Override
 	public void onDisable(){
 		
+	}
+	
+	@EventHandler(priority = EventPriority.NORMAL)
+	public void onPlayerJoin(PlayerJoinEvent event){
+		Player player = event.getPlayer();
+		String date = new SimpleDateFormat("MM/dd/yyyy").format(Calendar.getInstance());
+		Spinalpack.updateTrafficRecord(date, Spinalpack.checkUniqueTraffic(player.getName()));
 	}
 }
